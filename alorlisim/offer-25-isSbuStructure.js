9a575a856de85e27b3f1dcdba978714bc09f98ee
jiabaoliang <jiabaoliang@tencent.com>
2020-09-07 12:22:03 +0800

    feat: update

--- alorlisim/offer-25-isSbuStructure.js
+++ alorlisim/offer-25-isSbuStructure.js
@@ -1,38 +1,13 @@
 // 树的子结构
 
 function isSubStructure (a, b) {
-
+  if (!a || !b) return false
+  return isSameTree(a, b) || isSubStructure(a.left, b) || isSubStructure(a.right, b)
 }
 
 function isSameTree (p, q) { // 深度优先
-  let l1 = p.left
-  let l2 = q.left
-  let lr = true
-  while (l1 && l2) {
-    if (!checkData(p.left, q.left)) {
-      lr = false
-      return lr
-    }
-    l1 = l1.left
-    l2 = l2.left
-    lr = isSameTree(l1, l2)
-  }
-  let r1 = p.right
-  let r2 = q.right
-  let rr = true
-  while (r1 && r2) {
-    if (!checkData(p.left, q.left)) {
-      rr = false
-      return false
-    }
-    r1 = r1.left
-    r2 = r2.left
-  }
-  if (lr && rr && p.val === q.val) {
-    return true
-  }
-  return false
-}
-function checkData (val1, val2) {
-  return val1.val === val2.val
+  if (q === null) return true
+  if (p === null) return false
+  if (p.val !== q.val) return false
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 }
